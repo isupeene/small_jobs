f41ac674ea15d0e0dab6e9448ba8a1042e3e7d2b
Isaac Supeene <isupeene@ualberta.ca>
2015-02-25 19:25:21 +0000

    Removed references to the python-rauth package which is not required for our project.

--- web/job_posting/views.py
+++ web/job_posting/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 
-from oauth2_provider.views.generic import ProtectedResourceView
-from oauth2_provider.decorators import protected_resource
-
 from small_jobs_api.decorators import require_login
 
 
