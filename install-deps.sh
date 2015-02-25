67a4cbeb08488c6ed1b8ef785041cd11310ba0f0
Isaac Supeene <isupeene@ualberta.ca>
2015-02-25 19:22:23 +0000

    Added python-openid to the install-deps script.

--- install-deps.sh
+++ install-deps.sh
@@ -2,3 +2,15 @@ apt-get install postgresql
 apt-get install python-psycopg2
 apt-get install python-django
 
+cd $(dirname $0)/..
+
+if [-d python-openid]; then
+	cd python-openid
+	git pull
+else
+	git clone git@github.com:openid/python-openid.git
+	cd python-openid
+fi
+
+python setup.py install
+
