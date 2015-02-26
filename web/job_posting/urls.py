4f5c779ab4eb14b31c3d69eb6c8d451d484ae24e
Vince <vzphung@ualberta.ca>
2015-02-26 02:20:24 -0700

    simple form made , but doesnt do anything

--- web/job_posting/urls.py
+++ web/job_posting/urls.py
@@ -3,6 +3,10 @@ from job_posting import views
 
 
 urlpatterns = patterns('',
-	url(r'^protected/$', views.protected, name='protected')
+	url(r'^protected/$', views.protected, name='protected'),
+	url(r'^newjob/$', views.new_job, name='new job'),
+	url(r'^newjob/post_a_job', views.post_new_job, name='vote'),
+
 )
+
 
