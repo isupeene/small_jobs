763901601533ae3681bf1c3eb9826e23c8a57d69
Vince <vzphung@ualberta.ca>
2015-03-02 22:12:20 -0700

    made simple html templates, posting jobs and editing jobs work with hard coded profile

--- web/small_jobs/settings.py
+++ web/small_jobs/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'small_jobs_api'
+	'small_jobs_api',
+    'job_posting'
 )
 
 MIDDLEWARE_CLASSES = (
