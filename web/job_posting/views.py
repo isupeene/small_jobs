839d2753cd8e70a231ad8c84ebbd79c1a8ef2648
Vince <vzphung@ualberta.ca>
2015-03-02 22:12:20 -0700

    made simple html templates, posting jobs and editing jobs work with hard coded profile

--- web/job_posting/views.py
+++ web/job_posting/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
+from django.utils.timezone import now
+from django.core.serializers import serialize
+from datetime import timedelta
 
 
 from small_jobs_api.decorators import require_login
@@ -18,18 +21,46 @@ from small_jobs_api.api import (
 def protected(request):
 	return HttpResponse("Hello, World!")
 
-def new_job(request):
-	context =  {'superman': "I'm super!"}
-	return render(request,'job_posting/index.html', context)
+#@require_login
+def homepage(request):
+	return render(request,'job_posting/homepage.html')
 
-@require_login
+def mainpage(request):
+	return render(request,'job_posting/mainpage.html')
+
+def edit_profile(request):
+	return render(request,'job_posting/edit_profile.html')
+
+def create_job(request):
+	return render(request,'job_posting/create_job.html')
+
+def job_details(request):
+	return render(request,'job_posting/job_details.html')
+
+def jobs(request):
+	return render(request,'job_posting/jobs.html')
+
+def view_profile(request):
+	return render(request,'job_posting/view_profile.html')
+
+def new_job(request):
+	return render(request,'job_posting/index.html')
+
+
+# form stuff
 def post_new_job(request):
 	description = request.POST['description']
 	short_description = request.POST['short_description']
 	# bidding_deadline = request.POST['bidding_deadline']
 	# compensation_amount = request.POST['compensation_amount']
-	# TODO need to ge the id prob 
-	JobPoster(name="Bob", openid="0")
+	# TODO change all of this hardcoded stuff! 
+	dave = JobPoster.objects.get(name="Dave")
+	email = "bob@cableguy.com"
+	dave.email = email
+	update_job_poster(dave)
+	dave.openid = 5
+	dave.description = "Every group needs a Dave"
+	dave.phonenumber = "123456789"
 	myPosting = JobPosting(
 			description=description,
 			short_description=short_description,
@@ -37,7 +68,15 @@ def post_new_job(request):
 			bidding_confirmation_deadline=now() + timedelta(days=15),
 			bid_includes_compensation_amount = False,
 			bid_includes_completion_date = False,
-			**kwargs
 		)
-	create_job_posting(myUser, myPosting)
-	return HttpResponse(short_description)
+	create_job_posting(dave, myPosting)
+	return render(request, 'job_posting/jobs.html')
+
+def edit_my_profile_form(request):
+	# name = request.POST['name']
+	dave = JobPoster.objects.get(name="Dave")
+	dave.description = request.POST['description']
+	dave.email = request.POST['email']
+	dave.phonenumber = request.POST['phone_number']
+	update_job_poster(dave)
+	return HttpResponse(serialize("json", JobPoster.objects.all()))
